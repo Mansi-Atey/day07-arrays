4acda65418a966f75548bdba7945fc1b65109039
unknown <you@example.com>
2021-02-20 00:08:13 +0530

    Addded: code to find prime factors

--- array.sh
+++ array.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
-min=0
-max=0
-arr=(5 1 8 3 9 2)
-echo "Array in original order"
-echo ${arr[@]}
-for ((i=0;i<6;i++))
-do
-  for((j=0;j<6-i-1;j++))
-do
-  if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
-then
-temp=${arr[j]}
-arr[$j]=${arr[$((j+1))]}
-arr[$((j+1))]=$temp
-fi
-done
-done
-echo "Array in sorted order is:="
-echo ${arr[@]}
-for i in ${arr[@]}
-do
- (( $i > max || max == 0)) && max=$i
- (( $i < min || min == 0)) && min=$i
-done
-echo min=$min
-echo max=$max
+echo "Enter a number"
+ read n
+count=0
+for ((i=2;i<=$n;i++))
+ do
+   while [ $(($n%$i)) -eq 0 ]
+  do
+ arr[$count]=$i
+n=$(($n/$i))
+count=$count+1
+   done
+ done
+echo "The prime factors are:=" ${arr[@]}
+
