2a889109aa86686d9f7173c69807d0808fd0ee23
unknown <you@example.com>
2021-02-20 00:02:48 +0530

    Addded: code to find 2nd largest and smallest number in array

--- array.sh
+++ array.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 min=0
 max=0
-MAXCOUNT=10
-count=1
-
-
-while [ "$count" -le $MAXCOUNT ]; do
-randomNum=$((100 + $RANDOM%1000))
-
-number[$count]=$randomNum
- let "count += 1"
+arr=(5 1 8 3 9 2)
+echo "Array in original order"
+echo ${arr[@]}
+for ((i=0;i<6;i++))
+do
+  for((j=0;j<6-i-1;j++))
+do
+  if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+then
+temp=${arr[j]}
+arr[$j]=${arr[$((j+1))]}
+arr[$((j+1))]=$temp
+fi
 done
-
-echo "${number[@]}"
-
-
-for i in ${number[@]}
+done
+echo "Array in sorted order is:="
+echo ${arr[@]}
+for i in ${arr[@]}
 do
  (( $i > max || max == 0)) && max=$i
  (( $i < min || min == 0)) && min=$i
